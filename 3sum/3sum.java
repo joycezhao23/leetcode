7394abb8b617c0002e2f4f2f8b979950660dd601
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-05-03 17:57:41 -0700

    Time: 22 ms (51.61%), Space: 43.1 MB (50.71%) - LeetHub

---
+++ 3sum/3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    j++;
+                } else if (nums[j] + nums[k] == -nums[i]) {
+                    List<Integer> sol = new ArrayList<Integer>();
+                    sol.add(nums[i]);
+                    sol.add(nums[j]);
+                    sol.add(nums[k]);
+                    res.add(sol);
+                    j++;
+                    k--;
+                } else if (nums[j] + nums[k] > -nums[i]) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
