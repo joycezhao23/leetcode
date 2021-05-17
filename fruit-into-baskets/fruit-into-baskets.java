0834d4318ff3e1a681dcd2e23d693b75dd7469d7
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-05-17 07:54:13 -0700

    Time: 39 ms (46.20%), Space: 47.3 MB (48.13%) - LeetHub

--- fruit-into-baskets/fruit-into-baskets.java
+++ fruit-into-baskets/fruit-into-baskets.java
@@ -1,23 +1,19 @@
 class Solution {
     public int totalFruit(int[] tree) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int res = 0, n = tree.length, lo = 0;
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(tree[i]) || map.size() < 2) {
-                map.put(tree[i], map.getOrDefault(tree[i], 0) + 1);
-            } else {
-                res = Math.max(i - lo, res);
-                while (map.size() == 2) {
-                    int key = tree[lo++];
-                    if (map.get(key) == 1) {
-                        map.remove(key);
-                    } else {
-                        map.put(key, map.get(key) - 1);    
-                    }
-                }
-                map.put(tree[i], 1);
+        int sum = 0, res = 0, l = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < tree.length; i++) {
+            map.put(tree[i], map.getOrDefault(tree[i], 0) + 1);
+            sum++;
+            while (map.size() > 2) {
+                map.put(tree[l], map.get(tree[l]) - 1);
+                map.remove(tree[l++], 0);
+                sum--;
             }
+            res = Math.max(res, sum);
         }
-        return Math.max(res, n - lo);
+        
+        return res;
     }
 }
