cb29ef51175377b00dc989187ec83066be4f56e3
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-05-17 08:06:07 -0700

    Time: 1 ms (100.00%), Space: 42.1 MB (64.12%) - LeetHub

--- binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -3,15 +3,15 @@ class Solution {
         return atMost(nums, goal) - atMost(nums, goal - 1);
     }
     
-    private int atMost(int[] nums, int g) {
-        if (g < 0) return 0;
-        int res = 0, i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            g -= nums[j];
-            while (g < 0) {
-                g += nums[i++];
+    private int atMost(int[] nums, int t) {
+        if (t < 0) return 0;
+        int l = 0, sum = 0, res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while (sum > t) {
+                sum -= nums[l++]; 
             }
-            res += j - i + 1;
+            res += i - l + 1;
         }
         return res;
     }
