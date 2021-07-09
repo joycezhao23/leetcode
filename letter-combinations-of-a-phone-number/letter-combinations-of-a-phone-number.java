d178d5266f03fdba33029dde1fffaba9821b1d0c
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-07-08 20:39:34 -0700

    Time: 1 ms (71.09%), Space: 37.7 MB (83.14%) - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<String>();
+        if (digits.length() == 0) return res;
+        Map<Character, char[]> map = new HashMap<Character, char[]>();
+        map.put('2', new char[]{'a', 'b', 'c'});
+        map.put('3', new char[]{'d', 'e', 'f'});
+        map.put('4', new char[]{'g', 'h', 'i'});
+        map.put('5', new char[]{'j', 'k', 'l'});
+        map.put('6', new char[]{'m', 'n', 'o'});
+        map.put('7', new char[]{'p', 'q', 'r', 's'});
+        map.put('8', new char[]{'t', 'u', 'v'});
+        map.put('9', new char[]{'w', 'x', 'y', 'z'});
+        
+        char[] cs = digits.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        helper(cs, 0, sb, res, map);
+        return res;
+    }
+    
+    private void helper(char[] cs, int pos, StringBuilder sb, List<String> res, Map<Character, char[]> map) {
+        if (pos == cs.length) {
+            res.add(sb.toString());
+            return;
+        }
+        
+        for (char c : map.get(cs[pos])) {
+            sb.append(c);
+            helper(cs, pos + 1, sb, res, map);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
