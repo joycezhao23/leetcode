80cba223f659c976178a2e3fa0b2810360212a6f
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-08-31 20:40:40 -0700

    Time: 6 ms (9.09%), Space: 44.7 MB (8.65%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,35 +1,50 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        rows = grid.length;
+        cols = grid[0].length;
+        int[] parents = new int[rows * cols];
+        int[] dx = new int[]{0, -1};
+        int[] dy = new int[]{-1, 0};
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '0')
                     continue;
+                int idx = convert1d(i, j);
+                parents[idx] = idx;
                 count++;
-                bfs(grid, i, j);
+                for (int k = 0; k < 2; k++) {
+                    int x = i + dx[k], y = j + dy[k];
+                    if (!isValid(x, y, grid))
+                        continue;
+                    union(parents, idx, convert1d(x, y));
+                }
             }
         }
         return count;
     }
     
-    private void bfs(char[][] grid, int x, int y) {
-        int[] dx = {0,1,0,-1}, dy = {1,0,-1,0};
-        Queue<int[]> q = new LinkedList<int[]>();
-        q.add(new int[]{x, y});
-        grid[x][y] = '0';
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int xx = cur[0] + dx[i], yy = cur[1] + dy[i];
-                if (isValid(grid, xx, yy)) {
-                    grid[xx][yy] = '0';
-                    q.add(new int[]{xx, yy});
-                }
-            }
+    int count = 0;
+    int rows = 0, cols = 0;
+    
+    private int find(int[] parents, int x) {
+        if (parents[x] == x)
+            return x;
+        return parents[x] = find(parents, parents[x]);
+    }
+    
+    private void union(int[] parents, int x, int y) {
+        int px = find(parents, x), py = find(parents, y);
+        if (px != py) {
+            count--;
+            parents[px] = py;
         }
     }
     
-    private boolean isValid(char[][] grid, int x, int y) {
-        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == '1';
+    private int convert1d(int x, int y) {
+        return x * cols + y;
+    }
+    
+    private boolean isValid(int x, int y, char[][] grid) {
+        return x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == '1';
     }
 }
