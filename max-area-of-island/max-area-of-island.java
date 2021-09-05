89a517561dfefcabc8321cccc6aad6b79816dac8
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-09-05 16:10:35 -0700

    Time: 2 ms (99.50%), Space: 39.9 MB (37.58%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,30 +1,27 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int res = 0, m = grid.length, n = grid[0].length;
-        int[] dx = {0,1,0,-1};
-        int[] dy = {1,0,-1,0};
-        boolean[][] visited = new boolean[m][n];
-        Queue<int[]> q = new LinkedList<int[]>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j]) continue;
-                visited[i][j] = true;
-                q.add(new int[]{i, j});
-                int count = 0;
-                while (!q.isEmpty()) {
-                    int[] cur = q.poll();
-                    if (grid[cur[0]][cur[1]] == 0) continue;
-                    count++;
-                    for (int k = 0; k < 4; k++) {
-                        int x = cur[0] + dx[k], y = cur[1] + dy[k];
-                        if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) continue;
-                        q.add(new int[]{x, y});
-                        visited[x][y] = true;
-                    }
-                }
-                res = Math.max(count, res);
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0)
+                    continue;
+                res = Math.max(res, dfs(grid, i, j));
             }
         }
-        return res;        
+        return res;
+    }
+    
+    int[] dx = {0,1,0,-1}, dy = {1,0,-1,0};
+    
+    private int dfs(int[][] grid, int x, int y) {
+        int count = 1;
+        grid[x][y] = 0;
+        for (int i = 0; i < 4; i++) {
+            int xx = x + dx[i], yy = y + dy[i];
+            if (xx >= 0 && xx < grid.length && yy >= 0 && yy < grid[0].length && grid[xx][yy] == 1) {
+                count += dfs(grid, xx, yy);
+            }
+        }
+        return count;
     }
 }
