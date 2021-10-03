0b5581a0b11cde6ff4ef90254a94338305069bbd
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-10-02 18:30:45 -0700

    Time: 34 ms (46.24%), Space: 56.9 MB (17.20%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
-        for (int i = 0; i < n - 2; i++) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (n < 3) return res;
+        
+        for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1, k = n - 1;
-            while (j < k) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    j++;
-                } else if (nums[j] + nums[k] == -nums[i]) {
+            int l = i + 1, r = n - 1, target = 0 - nums[i];
+            while (l < r) {
+                if (nums[l] + nums[r] == target) {
                     List<Integer> sol = new ArrayList<Integer>();
                     sol.add(nums[i]);
-                    sol.add(nums[j]);
-                    sol.add(nums[k]);
+                    sol.add(nums[l]);
+                    sol.add(nums[r]);
                     res.add(sol);
-                    j++;
-                    k--;
-                } else if (nums[j] + nums[k] > -nums[i]) {
-                    k--;
+                    while (l < n - 1 && nums[l] == nums[l + 1]) l++;
+                    while (r > 0 && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                } else if (nums[l] + nums[r] < target) {
+                    while (l < n - 1 && nums[l] == nums[l + 1]) l++;
+                    l++;
                 } else {
-                    j++;
+                    while (r > 0 && nums[r] == nums[r - 1]) r--;
+                    r--;
                 }
             }
         }
