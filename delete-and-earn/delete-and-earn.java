c2873836a9fc0050e3a80dce774f3ee3139dd965
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-10-07 08:07:23 -0700

    Time: 8 ms (20.24%), Space: 38.8 MB (57.06%) - LeetHub

--- delete-and-earn/delete-and-earn.java
+++ delete-and-earn/delete-and-earn.java
@@ -1,28 +1,23 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        List<Integer> dedup = new ArrayList<Integer>();
-        for (int num : nums) {
-            if (!map.containsKey(num)) {
-                dedup.add(num);
-                map.put(num, 0);
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        List<Integer> num = new ArrayList<Integer>();
+        for (int n : nums) {
+            if (!count.containsKey(n)) {
+                num.add(n);
+                count.put(n, 0);
             }
-            map.put(num, map.get(num) + 1);
+            count.put(n, count.get(n) + 1);
         }
-        Collections.sort(dedup);
-        int n = dedup.size();
-        int[][] dp = new int[n + 1][2];
-        dp[1][1] = map.get(dedup.get(0)) * dedup.get(0);
-        for (int i = 1; i < n; i++) {
-            if (dedup.get(i - 1) == dedup.get(i) - 1) {
-                dp[i + 1][0] = dp[i][1];
-                dp[i + 1][1] = Math.max(dp[i - 1][1], dp[i - 1][0]) + map.get(dedup.get(i)) * dedup.get(i);    
-            } else {
-                dp[i + 1][0] = Math.max(dp[i][1], dp[i][0]);
-                dp[i + 1][1] = Math.max(dp[i][1], dp[i][0]) + map.get(dedup.get(i)) * dedup.get(i);
-            }
-            
+        Collections.sort(num);
+        int size = num.size();
+        int[][] dp = new int[size][2];
+        dp[0][1] = num.get(0) * count.get(num.get(0));
+        for (int i = 1; i < size; i++) {
+            int cur = num.get(i), prev = num.get(i - 1);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = (cur == prev + 1) ? (dp[i - 1][0] + cur * count.get(cur)) : (dp[i][0] + cur * count.get(cur));
         }
-        return Math.max(dp[n][0], dp[n][1]);
+        return Math.max(dp[size - 1][0], dp[size - 1][1]);
     }
 }
