085762629ef4cefe3d523374f7bb69e5d16a56e5
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-10-08 20:21:26 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (75.36%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        int n = nums.length, count = (1 << n);
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < (1 << nums.length); i++) {
             List<Integer> sol = new ArrayList<Integer>();
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) > 0) sol.add(nums[j]);
+            for (int j = 0; j < nums.length; j++) {
+                if ((i & (1 << j)) != 0) 
+                    sol.add(nums[j]);
             }
             res.add(sol);
         }
