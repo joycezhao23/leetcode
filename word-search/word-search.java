7fd92728964a25d0fc8f701234b7c7fe94e8a0ff
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-10-11 07:45:36 -0700

    Time: 254 ms (14.27%), Space: 38.8 MB (30.98%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -1,32 +1,31 @@
 class Solution {
-    char[][] b;
-    int m, n;
     public boolean exist(char[][] board, String word) {
-        b = board;
+        this.board = board;
         m = board.length;
         n = board[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (helper(i, j, 0, word)) return true;
+                if (helper(i, j, 0, word)) 
+                    return true;
             }
         }
         return false;
     }
-    
+    char[][] board;
+    int m, n;
     int[] dx = {0,1,0,-1}, dy = {1,0,-1,0};
     private boolean helper(int x, int y, int idx, String word) {
         if (idx == word.length()) return true;
-        if (x < 0 || x >= m || y < 0 || y >= n || b[x][y] != word.charAt(idx))
-            return false;
-        char c = b[x][y];
-        b[x][y] = '0';
+        if (x < 0 || x >= m || y < 0 || y >= n) return false;
+        if (board[x][y] != word.charAt(idx)) return false;
+        char c = board[x][y];
+        board[x][y] = '.';
         for (int i = 0; i < 4; i++) {
             int xx = x + dx[i], yy = y + dy[i];
-            if (helper(xx, yy, idx + 1, word)) {
-                return true;   
-            }
+            if (helper(xx, yy, idx + 1, word))
+                return true;
         }
-        b[x][y] = c;
+        board[x][y] = c;
         return false;
     }
 }
