de10b51ef41c9560452471edb079674e0623c322
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-10-31 18:38:53 -0700

    Time: 1 ms (99.82%), Space: 41.1 MB (76.25%) - LeetHub

--- surrounded-regions/surrounded-regions.java
+++ surrounded-regions/surrounded-regions.java
@@ -1,35 +1,32 @@
 class Solution {
-    int m, n;
     public void solve(char[][] board) {
         m = board.length;
         n = board[0].length;
-        if (m < 3 || n < 3) return;
-        for (int i = 0; i < n; i++) {
-            dfs(0, i, board);
-            dfs(m - 1, i, board);
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(i, 0, board);
+            if (board[i][n - 1] == 'O') dfs(i, n - 1, board);
         }
-        for (int j = 1; j < m - 1; j++) {
-            dfs(j, 0, board);
-            dfs(j, n - 1, board);
+        for (int i = 1; i < n - 1; i++) {
+            if (board[0][i] == 'O') dfs(0, i, board);
+            if (board[m - 1][i] == 'O') dfs(m - 1, i, board);
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == '.')
-                    board[i][j] = 'O';
-                else if (board[i][j] == 'O')
-                    board[i][j] = 'X';
+                if (board[i][j] == '.') board[i][j] = 'O';
+                else if (board[i][j] == 'O') board[i][j] = 'X';
             }
         }
     }
-    
     int[] dx = {0,1,0,-1}, dy = {1,0,-1,0};
-    private void dfs(int x, int y, char[][] b) {
-        if (b[x][y] != 'O') return;
-        b[x][y] = '.';
+    int m, n;
+    
+    private void dfs(int x, int y, char[][] board) {
+        board[x][y] = '.';
         for (int i = 0; i < 4; i++) {
             int xx = x + dx[i], yy = y + dy[i];
-            if (xx < 0 || xx >= m || yy < 0 || yy >= n) continue;
-            dfs(xx, yy, b);
+            if (xx < 0 || xx >= m || yy < 0 || yy >= n || board[xx][yy] != 'O')
+                continue;
+            dfs(xx, yy, board);
         }
     }
 }
