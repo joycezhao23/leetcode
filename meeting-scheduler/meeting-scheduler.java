49bd595191ddc78ef1daffe4fe4db3aaacbe0ba1
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-11-02 20:34:57 -0700

    Time: 25 ms (74.78%), Space: 48.5 MB (52.80%) - LeetHub

--- meeting-scheduler/meeting-scheduler.java
+++ meeting-scheduler/meeting-scheduler.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
-        Arrays.sort(slots1, (a, b) -> a[0] - b[0]);
-        Arrays.sort(slots2, (a, b) -> a[0] - b[0]);
+        Arrays.sort(slots1, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        Arrays.sort(slots2, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int p1 = 0, p2 = 0, n1 = slots1.length, n2 = slots2.length;
         List<Integer> res = new ArrayList<Integer>();
-        int i = 0, j = 0;
-        while (i < slots1.length && j < slots2.length) {
-            int l = Math.max(slots1[i][0], slots2[j][0]);
-            int r = Math.min(slots1[i][1], slots2[j][1]);
-            if ((long)r - l >= duration) {
+        while (p1 < n1 && p2 < n2) {
+            int l = Math.max(slots1[p1][0], slots2[p2][0]);
+            int r = Math.min(slots1[p1][1], slots2[p2][1]);
+            if (r - l >= duration) {
                 res.add(l);
                 res.add(l + duration);
-                break;
+                return res;
             }
-            if (r == slots1[i][1]) {
-                i++;
-            } else {
-                j++;
-            }
+            if (slots1[p1][1] < slots2[p2][1]) 
+                p1++;
+            else 
+                p2++;
         }
         return res;
     }
