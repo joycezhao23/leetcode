8bed01e5b44939c39ee0975aaac74d32406fc0f0
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2021-11-04 18:31:58 -0700

    Time: 1 ms (90.74%), Space: 39 MB (49.14%) - LeetHub

--- trapping-rain-water/trapping-rain-water.java
+++ trapping-rain-water/trapping-rain-water.java
@@ -1,22 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int l = 0, r = height.length - 1, total = 0, n = height.length;
+        int l = 0, r = height.length - 1, res = 0;
         while (l < r) {
-            while (l < n - 1 && height[l] <= height[l + 1]) l++;
-            while (r > 0 && height[r] <= height[r - 1]) r--;
+            while (l < r && height[l] <= height[l + 1])
+                l++;
+            while (l < r && height[r] <= height[r - 1])
+                r--;
             if (l >= r) break;
-            
-            int h = Math.min(height[r], height[l]);
-            if (height[l] <= height[r]) {
+            if (height[l] < height[r]) {
+                int h = height[l];
                 while (l < r && height[l] <= h) {
-                    total += h - height[l++];
+                    res += h - height[l++];
                 }
+                
             } else {
-                while (l < r && height[r] <= h) {
-                    total += h - height[r--];
-                }
+                int h = height[r];
+                while (r > l && height[r] <= h)
+                    res += h - height[r--];
             }
         }
-        return total;
+        return res;
     }
 }
