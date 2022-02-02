55a3e012f16337607790f1cc45ab1431db32bfae
joycezhao23 <37035805+joycezhao23@users.noreply.github.com>
2022-02-01 19:10:55 -0800

    Time: 10 ms (74.68%), Space: 47.7 MB (17.48%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (s.length() < p.length()) return res;
+        int[] count = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            count[p.charAt(i) - 'a']++;
+            count[s.charAt(i) - 'a']--;
+        }
+        int l = p.length();
+        if (isValid(count)) res.add(0);
+        for (int i = p.length(); i < s.length(); i++) {
+            count[s.charAt(i) - 'a']--;
+            count[s.charAt(i - l) - 'a']++;
+            if (isValid(count))
+                res.add(i - l + 1);
+        }
+        return res;
+    }
+    
+    private boolean isValid(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
